019939fdc3c67edb4e4dfa298c84d1853692ce70
Zhen Lu <lv.zhen.g@gmail.com>
2012-02-17 10:22:46 -0800

    added insert test case 01

--- fundamentals/AugTreeSet/AugTreeSet/main.cpp
+++ fundamentals/AugTreeSet/AugTreeSet/main.cpp
@@ -1,30 +1,55 @@
 #include <iostream>
+#include <ctime>
+#include <set>
 #include "AugTreeSet.h"
 
-int main()
+// Helper function to keep the console window open
+void press_enter_to_close()
+{
+    do
+    {
+        std::cout << "Press the ENTER key" << std::endl;
+    } while(std::cin.get() != '\n');
+}
+
+int main( int argc, char** argv )
 {
     using namespace augment_data_structure;
 
     // test case in a scope
     AugTreeSet<int> augTestSet;
-    for (int i=0; i<INT_MAX; ++i)
-    {
-        std::cout << i << std::endl;
-        augTestSet.Insert(i);
+    std::set<int> stdTestSet;
+    int testRunLimit = INT_MAX/256;
+    time_t startTime;
+    time_t endTime;
+    time_t timeDiff;
 
-        if (i % 3 == 1)
-        {
-            int medin;
-            if (augTestSet.GetNthItm(i/2+1, medin))
-            {
-                std::cout << "The " << i/2+1 << "th item is: " << medin << std::endl;
-                if (augTestSet.Remove(medin))
-                {
-                    std::cout << "and removed" << std::endl;
-                }
-            }
-        }
+    //////////////////////////////////////////////////////////////////////////
+    // INSERT TESTS
+    //////////////////////////////////////////////////////////////////////////
+    // Insert tests -------- AugSet
+    startTime = time(NULL);
+    for (int i=0; i<testRunLimit; ++i)
+    {
+        stdTestSet.insert(i);
     }
+    endTime = time(NULL);
+    timeDiff = (endTime - startTime);
+    std::cout << "std lib set insert done used time:   " << timeDiff << std::endl;
+
+    // Insert tests -------- AugSet
+    startTime = time(NULL);
+    for (int i=0; i<testRunLimit; ++i)
+    {
+        augTestSet.Insert(i);
+    }
+    endTime = time(NULL);
+    timeDiff = (endTime - startTime);
+    std::cout << "AugSet insert done used time:   " << timeDiff << std::endl;
+
+    //////////////////////////////////////////////////////////////////////////
+    // press_enter_to_close();
+    //////////////////////////////////////////////////////////////////////////
 
     return 0;
 }
