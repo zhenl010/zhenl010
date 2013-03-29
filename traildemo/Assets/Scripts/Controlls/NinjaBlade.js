5175894421fd356371fda2417840494f32496464
Zhen Lu <lv.zhen.g@gmail.com>
2013-03-29 15:39:28 -0700

    removed traildemo

--- traildemo/Assets/Scripts/Controlls/NinjaBlade.js
+++
@@ -1,5 +0,0 @@
-function OnTriggerEnter( inCollider : Collider )
-{
-    // cut into sth? 1 is the type of the blade 
-    inCollider.SendMessage( "ReactToCut", 1, SendMessageOptions.DontRequireReceiver );
-}
